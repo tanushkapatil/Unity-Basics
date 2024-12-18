f6f60625b62e9b7e5c7ebb3e574d4b5953f4c455
Tanushka Patil <94859584+tanushkapatil@users.noreply.github.com>
2024-12-19 00:51:09 +0530

    Update and rename start&update.cs to basics.cs

--- basics.cs
+++ basics.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    float xValue = 0.001f ;
-    float yValue = 0f ;
-    float zValue = 0f ;
+    [SerializeField]float xValue = 0.001f ; //movement speed on that axis 
+    //SerializeField helps set value in unity
+    [SerializeField]float yValue = 0f ;
+    [SerializeField]float zValue = 0f ;
+    //To control the object + and - help in moving object towards or away from the camera
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
